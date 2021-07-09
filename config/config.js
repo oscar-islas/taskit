01e046a4312635bca8b208a20b3ab8c50360543e
oislasreyes <oislasreyes@gmail.com>
2021-07-08 21:03:41 -0500

    modificando la cadena de conexión de la DB para heroku

--- config/config.js
+++ config/config.js
@@ -24,10 +24,16 @@ module.exports = {
     }
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    use_env_variable: 'DATABASE_URL',
+    define: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+    }
   }
 }
